d9ad6c80a52dc876e6d6ad1100f25d79e4a047ff
Raymond Loman <raymond@carpago.nl>
2018-03-07 14:30:22 +0100

    First steps

---
+++ src/main/java/com/capgemini/bike/domain/Room.java
@@ -0,0 +1,29 @@
+package com.capgemini.bike.domain;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @ManyToMany(mappedBy = "rooms")
+    private Set<Booking> bookings = new HashSet<>();
+
+    public long getId() {
+        return id;
+    }
+
+    public Set<Booking> getBookings() {
+        return bookings;
+    }
+
+    public void addBooking(Booking b) {
+        this.bookings.add(b);
+        b.getRooms().add(this);
+    }
+}
