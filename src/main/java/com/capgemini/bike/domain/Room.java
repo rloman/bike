25c01631c60568708b6a7766bfad57d222746081
Raymond Loman <raymond@carpago.nl>
2018-03-07 14:55:10 +0100

    Add more which starts but not sure if it rocks

--- src/main/java/com/capgemini/bike/domain/Room.java
+++ src/main/java/com/capgemini/bike/domain/Room.java
@@ -11,12 +11,23 @@ public class Room {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
+    private short roomNumber;
+
     @ManyToMany(mappedBy = "rooms")
     private Set<Booking> bookings = new HashSet<>();
 
     public long getId() {
         return id;
     }
+
+    public short getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(short roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
 
     public Set<Booking> getBookings() {
         return bookings;
--- src/main/java/com/capgemini/bike/repositories/RoomRepository.java
+++ src/main/java/com/capgemini/bike/repositories/RoomRepository.java
@@ -1,12 +1,14 @@
 package com.capgemini.bike.repositories;
 
 import com.capgemini.bike.domain.Room;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 
 @Repository
-public interface RoomRepository {
+public interface RoomRepository extends CrudRepository<Room, Long> {
 
-    Room findWhereBookingCheckoutDate(LocalDate date);
+    Room findByBookingsCheckoutDate(LocalDate date);
+    Room findByBookingsCheckoutDateLessThanAndBookingsCheckinDateGreaterThan(LocalDate checkout, LocalDate checkin);
 }
