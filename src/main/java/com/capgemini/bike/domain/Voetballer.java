cbb585ee8ae5b80a7112376d54ab0b41c09ea7c3
Raymond Loman <raymond@carpago.nl>
2018-03-12 16:36:25 +0100

    Add some beans for demo purposes

---
+++ src/main/java/com/capgemini/bike/domain/Voetballer.java
@@ -0,0 +1,14 @@
+package com.capgemini.bike.domain;
+
+public class Voetballer {
+
+    private String name;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
