614e865d73ebbd4e4e83c1c830def03fd484cab7
Raymond Loman <raymond@carpago.nl>
2018-03-19 11:34:45 +0100

    Some review

---
+++ src/main/java/com/capgemini/bike/util/StringTrainer.java
@@ -0,0 +1,61 @@
+package com.capgemini.bike.util;
+
+
+class Person {
+
+	public final String name;
+	public final int age;
+
+	public int andereAge;
+
+	public Person(String name, int age) {
+
+		this.name = name;
+		this.age = age;
+	}
+
+
+	public Person() {
+		this.name = "";
+		this.age = 999;
+
+	}
+
+}
+
+public class StringTrainer {
+
+
+
+
+	public static void main(String[] args) {
+
+		int f1;
+
+		int f;
+
+
+
+		int a;
+		int $;
+
+		int _$aap;
+
+//		int -drie;
+
+		final int AANTAL=5;
+
+//		AANTAL++;
+
+		System.out.println(AANTAL);
+
+
+
+		final Person p = new Person("Richard", 16);
+
+//		p.name = "Muriel";
+
+//		p = new Person("Frank", 43);
+
+	}
+}
